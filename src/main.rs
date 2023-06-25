474e3ce134f937c57f8604a5fe9cd05edfefb6af
suap-dev <suap.dev@gmail.com>
2023-06-25 02:07:09 +0200

    triangle lives. rotation is done naively.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,136 @@
 #[macro_use]
 extern crate glium;
 
+#[derive(Copy, Clone, Debug)]
+struct Vertex {
+    position: [f32; 2],
+}
+implement_vertex!(Vertex, position);
+
 fn main() {
     use glium::{glutin, Surface};
+    use std::f32::consts::TAU;
 
+    // init Display
     let event_loop = glutin::event_loop::EventLoop::new();
-
     let window_builder = glutin::window::WindowBuilder::new();
     let context_builder = glutin::ContextBuilder::new();
-
     let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
-    
+
+    // first triangle
+    let default_position: [f32; 2] = rotated(&[0.0, 0.5], TAU / 7.0);
+    let v1 = Vertex {
+        position: rotated(&default_position, 0.0 * TAU / 3.0),
+    };
+    let v2 = Vertex {
+        position: rotated(&default_position, 1.0 * TAU / 3.0),
+    };
+    let v3 = Vertex {
+        position: rotated(&default_position, 2.0 * TAU / 3.0),
+    };
+    let mut triangle = vec![v1, v2, v3];
+
+    // vertex buffer (with our triangle)
+    let mut vertex_buffer = glium::VertexBuffer::new(&display, &triangle).unwrap();
+
+    // dummy marker - no idea...
+    // as I understood it's very important if you have more triangles. ?
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    // SHADERS
+    // Vertex Shader
+    // in vec2 position; - declare that we are expected to be passed an attribute named position
+    let vertex_shader_src = r#"
+        ##version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    // Fragment Shader
+
+    let fragment_shader_src = r#"
+        ##version 140
+
+        varying out vec4 color;
+
+        void main() {
+            color = vec4(0.0, 0.4, 0.7, 1.0);
+        }
+    "#;
+
+    // program
+    // send shaders to glium
+    let program =
+        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+            .unwrap();
+
+    // event loop (game loop?)
     event_loop.run(move |event, _, control_flow| {
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
         if let glutin::event::Event::WindowEvent { event, .. } = event {
             if event == glutin::event::WindowEvent::CloseRequested {
                 *control_flow = glutin::event_loop::ControlFlow::Exit;
+                return;
             }
         } else {
             return;
         }
 
+        // match event {
+        //     glutin::event::Event::WindowEvent { event, .. } => match event {
+        //         glutin::event::WindowEvent::CloseRequested => {
+        //             *control_flow = glutin::event_loop::ControlFlow::Exit;
+        //             return;
+        //         }
+        //         _ => return,
+        //     },
+        //     glutin::event::Event::NewEvents(cause) => match cause {
+        //         glutin::event::StartCause::ResumeTimeReached { .. } => (),
+        //         glutin::event::StartCause::Init => (),
+        //         _ => return,
+        //     },
+        //     _ => return,
+        // }
+
+        rotate_shape(&mut triangle, TAU / 1000.0);
+        vertex_buffer = glium::VertexBuffer::new(&display, &triangle).unwrap();
+        // println!("{:?}", triangle);
+
         let mut target = display.draw();
-        target.clear_color(1.0, 59.0/255.0, 0.0, 0.0);
+        target.clear_color(1.0, 59.0 / 255.0, 0.0, 0.0);
+        // draw prepared triangle with prepared program
+        target
+            .draw(
+                &vertex_buffer,
+                indices,
+                &program,
+                &glium::uniforms::EmptyUniforms,
+                &Default::default(),
+            )
+            .unwrap();
         target.finish().unwrap();
-    });            
+    });
+}
+
+fn rotated(vec2: &[f32; 2], angle: f32) -> [f32; 2] {
+    let x = vec2[0];
+    let y = vec2[1];
+
+    let new_x = x * angle.cos() - y * angle.sin();
+    let new_y = x * angle.sin() + y * angle.cos();
+
+    [new_x, new_y]
+}
+
+fn rotate_shape(triangle: &mut Vec<Vertex>, angle: f32) {
+    for v in triangle {
+        v.position = rotated(&v.position, angle);
+    }
 }
