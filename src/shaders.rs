601eed5be679cde1416d78baae28263d087fb3ce
suap-dev <suap.dev@gmail.com>
2023-06-30 16:58:49 +0200

    blinn-phong specular

--- src/shaders.rs
+++ src/shaders.rs
@@ -3,23 +3,19 @@ pub const VERTEX_SHADER: &str = r#"
 
 in vec3 position;
 in vec3 normal;
+
 out vec3 v_normal;
+out vec3 v_position;
 
 uniform mat4 u_perspective;
-// uniform mat4 u_view;
-// uniform mat4 u_translation;
-// uniform mat4 u_scale;
-// uniform mat4 u_rotation;
 uniform mat4 model_view;
 
-void main() {
-    // mat4 model_view = u_view * model;
-    // mat4 model_view = u_view * (u_translation * u_scale * u_rotation);
-    
+void main() {    
     // v_normal should be scaled if we non-uniformely scale positions.
-    v_normal = transpose(inverse(mat3(model_view))) * normal;
+    v_normal = transpose(inverse(mat3(model_view))) * normal;    
 
     gl_Position = u_perspective * model_view *  vec4(position, 1.0);
+    v_position = gl_Position.xyz / gl_Position.w;
 }
 "#;
 
@@ -27,14 +23,33 @@ pub const FRAGMENT_SHADER: &str = r#"
 #version 140
 
 in vec3 v_normal;
+in vec3 v_position;
+
 out vec4 color;
 
 uniform vec3 u_light;
 
+const vec3 ambient_color = vec3(0.30, 0.10, 0.20);
+const vec3 diffuse_color = vec3(0.80, 0.50, 0.60);
+const vec3 specular_color = vec3(1.0, 1.0, 1.0);
+
 void main() {
-    float brightness = dot(normalize(v_normal), normalize(u_light));
-    vec3 reg_color = vec3(0.80, 0.50, 0.60);
-    vec3 dark_color = vec3(0.30, 0.10, 0.20);
-    color = vec4(mix(dark_color, reg_color, brightness), 1.0);
+    vec3 light = normalize(u_light);
+    vec3 normal = normalize(v_normal);
+
+    vec3 position_direction = normalize(v_position);
+    vec3 camera_direction = -position_direction;
+
+    vec3 half_direction = normalize(light + camera_direction);
+
+    // dot product of 2 vectors is a cosine of the angle between them
+    float diffuse_intensity = max(dot(normal, light), -0.35);
+    float specular_intensity = pow(max(dot(half_direction, normal), 0.0), 16.0);
+
+    // vec3 scaled_ambient_color = ambient_color;
+    vec3 scaled_diffuse_color = diffuse_intensity * diffuse_color;
+    vec3 scaled_specular_color = specular_intensity * specular_color;
+
+    color = vec4(ambient_color + scaled_diffuse_color + scaled_specular_color, 1.0);
 }
 "#;
